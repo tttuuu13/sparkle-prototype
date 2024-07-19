7ae876da2b4c93aeba8b95727e7d8ee52f08ceab
tttuuu13 <tim.ib.222005@gmail.com>
2024-07-19 15:59:38 +0500

    Initial Commit

---
+++ cards/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cards
+//
+//  Created by тимур on 19.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cards/cardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardsApp.swift
+//  cards
+//
+//  Created by тимур on 19.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct cardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
